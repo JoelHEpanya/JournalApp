fa6be130974c17683901f2409325605e6e5d19b5
extra <extra@JOEL>
2025-03-13 21:39:44 +0000

    Replace QuickGrid with HTML table; add Blazorise
    Updated Music.razor to replace QuickGrid with an HTML table for displaying recent tracks, including columns for Track, Artist, Link, Rating, Notes, and Save. Imported Blazorise library and modified tracks list initialization for clarity.
    
    Updated JournalApp.csproj to include Blazorise.Bootstrap5 package reference (v1.7.5).
    
    Added migration files 20250224162610_mssql.local_migration_956.Designer.cs and 20250224162610_mssql.local_migration_956.cs to define and manage the structure of Dreams, Meals, and Notes entities in the database.

---
+++ JournalApp/Migrations/20250224162610_mssql.local_migration_956.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JournalApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqllocal_migration_956 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
